6c68fa33955a0f8ae54b249ea27b1727bf30f3bc
TsunYin-Lip-ConnectedLab <tlip+bose@connectedlab.com>
2017-10-16 18:03:11 -0400

    Completed session

--- app/src/test/java/com/connectedlab/kata3/RequestSenderTest.kt
+++ app/src/test/java/com/connectedlab/kata3/RequestSenderTest.kt
@@ -21,21 +21,58 @@ class RequestSenderTest {
 
     @Test
     fun sendRequestWithCallback() {
+        val callback = Mockito.mock(RequestCallback::class.java)
+
+        sut.sendRequestWithCallback(callback)
+
+        Mockito.verify(api).sendRequest(callback)
     }
 
     @Test
     fun sendRequestPrivateCallback() {
+        val captor = ArgumentCaptor.forClass(RequestCallback::class.java)
+
+        sut.sendRequestPrivateCallback()
+
+        Mockito.verify(api).sendRequest(captor.capture())
+        captor.value.onResponse("Server-Response")
+
+        assertEquals("modified Server-Response", sut.requestPrivateCallbackResult)
     }
 
     @Test
     fun sendRequestThrowsException() {
+        val callback = Mockito.mock(RequestCallback::class.java)
+        val error = IllegalStateException()
+        Mockito.`when`(api.sendRequest(callback)).thenThrow(error)
+
+        sut.sendRequestTryCatch(callback)
+
+        Mockito.verify(callback).onError(error)
     }
 
     @Test
     fun sendRequestWithParam() {
+        val captor = ArgumentCaptor.forClass(RequestCallback::class.java)
+
+        sut.sendRequestWithParam(2)
+
+        Mockito.verify(api).sendRequestWithParam(Mockito.eq(2), captor.capture())
+        captor.value.onResponse("Server-Response")
+
+        assertEquals("Server-Response:2", sut.requestWithParamResult)
     }
 
     @Test
     fun observeEvents() {
+        val emitter = EventEmitter()
+        Mockito.`when`(api.getEventEmitter()).thenReturn(emitter)
+
+        sut.observeEvents()
+
+        emitter.emitEvent()
+        
+        assertEquals(1, sut.eventsReceived)
+
     }
 }
